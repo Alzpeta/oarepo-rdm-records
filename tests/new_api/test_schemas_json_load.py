7b78e25772d67db9348c4c8e539180b2fb30976c
Alzbeta Pokorna <alzbeta.pokorna@cesnet.cz>
2020-09-17 16:40:10 +0200

    invenio tests

--- tests/new_api/test_schemas_json_load.py
+++ tests/new_api/test_schemas_json_load.py
@@ -276,58 +276,6 @@ def test_contributor():
     with pytest.raises(ValidationError):
         data = ContributorSchemaV1().load(invalid_no_role)
 
-
-
-def test_custom_contributor_role(config, vocabulary_clear):
-    """Test custom contributor role."""
-    prev_config = config['RDM_RECORDS_CUSTOM_VOCABULARIES']
-    config['RDM_RECORDS_CUSTOM_VOCABULARIES'] = {
-        'contributors.role': {
-            'path': os.path.join(
-                os.path.dirname(os.path.abspath(__file__)),
-                'data',
-                'contributor_role.csv'
-            )
-        }
-    }
-
-    # new contributor role validates
-    librarian_role = {
-        "name": "Julio Cesar",
-        "type": "Personal",
-        "given_name": "Julio",
-        "family_name": "Cesar",
-        "identifiers": {
-            "Orcid": "0000-0002-1825-0097",
-        },
-        "role": "Librarian"
-    }
-    data = ContributorSchemaV1().load(librarian_role)
-    assert data == librarian_role
-
-    # old contributor role does not validate
-    contactperson_role = {
-        "name": "Julio Cesar",
-        "type": "Personal",
-        "given_name": "Julio",
-        "family_name": "Cesar",
-        "identifiers": {
-            "Orcid": "0000-0002-1825-0097",
-        },
-        "affiliations": [{
-            "name": "Entity One",
-            "identifiers": {
-                "ror": "03yrm5c26"
-            }
-        }],
-        "role": "ContactPerson"
-    }
-    with pytest.raises(ValidationError):
-        data = ContributorSchemaV1().load(contactperson_role)
-
-    config['RDM_RECORDS_CUSTOM_VOCABULARIES'] = prev_config
-
-
 def test_internal_note():
     """Test internal note schema."""
     valid_full = {
@@ -368,180 +316,10 @@ def test_internal_note():
         data = InternalNoteSchemaV1().load(invalid_timestamp)
 
 
-def test_resource_type(vocabulary_clear):
-    """Test resource type."""
-    valid_full = {
-        "type": "image",
-        "subtype": "image-photo"
-    }
-    data = ResourceTypeSchemaV1().load(valid_full)
-    assert data == valid_full
-
-    valid_no_subtype = {
-        "type": "poster"
-    }
-    data = ResourceTypeSchemaV1().load(valid_no_subtype)
-    assert data == valid_no_subtype
-
-    invalid_no_subtype_when_required = {
-        "type": "image"
-    }
-    with pytest.raises(ValidationError):
-        ResourceTypeSchemaV1().load(invalid_no_subtype_when_required)
-
-    invalid_no_type = {
-        "subtype": "image-photo"
-    }
-    with pytest.raises(ValidationError):
-        ResourceTypeSchemaV1().load(invalid_no_type)
-
-    invalid_type = {
-        "type": "invalid",
-        "subtype": "image-photo"
-    }
-    with pytest.raises(ValidationError):
-        ResourceTypeSchemaV1().load(invalid_type)
-
-    invalid_subtype = {
-        "type": "image",
-        "subtype": "invalid"
-    }
-    with pytest.raises(ValidationError):
-        ResourceTypeSchemaV1().load(invalid_subtype)
-
-
-def test_custom_resource_type(config, vocabulary_clear):
-    """Test custom resource type."""
-    config['RDM_RECORDS_CUSTOM_VOCABULARIES'] = {
-        'resource_type': {
-            'path': os.path.join(
-                os.path.dirname(os.path.abspath(__file__)),
-                'data',
-                'resource_types.csv'
-            )
-        }
-    }
-
-    # new resource type validates
-    valid_full = {
-        "type": "my_image",
-        "subtype": "my_photo"
-    }
-    data = ResourceTypeSchemaV1().load(valid_full)
-    assert data == valid_full
-
-    # old resource type does not validate
-    valid_full = {
-        "type": "image",
-        "subtype": "image-photo"
-    }
-    with pytest.raises(ValidationError):
-        data = ResourceTypeSchemaV1().load(valid_full)
-
-    config['RDM_RECORDS_CUSTOM_VOCABULARIES'] = {}
-
-
-def test_title(vocabulary_clear):
-    """Test titles schema."""
-    valid_full = {
-        "title": "A Romans story",
-        "type": "Other",
-        "lang": "eng"
-    }
-    data = TitleSchemaV1().load(valid_full)
-    assert data == valid_full
-
-    valid_partial = {
-        "title": "A Romans story",
-        "lang": "eng"
-    }
-    data = TitleSchemaV1().load(valid_partial)
-    assert data == dict(valid_partial, type='MainTitle')
-
-    valid_minimal = {
-        "title": "A Romans story",
-    }
-    data = TitleSchemaV1().load(valid_minimal)
-    assert data == dict(valid_minimal, type='MainTitle')
-
-    invalid_no_title = {
-        "type": "Other",
-        "lang": "eng"
-    }
-    with pytest.raises(ValidationError):
-        data = TitleSchemaV1().load(invalid_no_title)
-
-    invalid_title_type = {
-        "title": "A Romans story",
-        "type": "Invalid",
-        "lang": "eng"
-    }
-    with pytest.raises(ValidationError):
-        data = TitleSchemaV1().load(invalid_title_type)
-
-    invalid_lang = {
-        "title": "A Romans story",
-        "type": "Other",
-        "lang": "inv"
-    }
-    with pytest.raises(ValidationError):
-        data = TitleSchemaV1().load(invalid_lang)
-
-
-def test_description():
-    """Test descriptions schema."""
-    valid_full = {
-        "description": "A story on how Julio Cesar relates to Gladiator.",
-        "type": "Abstract",
-        "lang": "eng"
-    }
-
-    data = DescriptionSchemaV1().load(valid_full)
-    assert data == valid_full
-
-    valid_minimal = {
-        "description": "A story on how Julio Cesar relates to Gladiator.",
-        "type": "Abstract"
-    }
-
-    data = DescriptionSchemaV1().load(valid_minimal)
-    assert data == valid_minimal
-
-    invalid_no_description = {
-        "type": "Abstract",
-        "lang": "eng"
-    }
-    with pytest.raises(ValidationError):
-        data = DescriptionSchemaV1().load(invalid_no_description)
-
-    invalid_no_description_type = {
-        "description": "A story on how Julio Cesar relates to Gladiator.",
-        "lang": "eng"
-    }
-    with pytest.raises(ValidationError):
-        data = DescriptionSchemaV1().load(invalid_no_description_type)
-
-    invalid_description_type = {
-        "description": "A story on how Julio Cesar relates to Gladiator.",
-        "type": "Invalid",
-        "lang": "eng"
-    }
-    with pytest.raises(ValidationError):
-        data = DescriptionSchemaV1().load(invalid_description_type)
-
-    invalid_lang = {
-        "description": "A story on how Julio Cesar relates to Gladiator.",
-        "type": "Abstract",
-        "lang": "inv"
-    }
-    with pytest.raises(ValidationError):
-        data = DescriptionSchemaV1().load(invalid_lang)
-
-
 def test_license():
     """Test license scehma."""
     valid_full = {
-        "license": "Copyright Maximo Decimo Meridio 2020. Long statement",
+        "license": {"en":"Copyright Maximo Decimo Meridio 2020. Long statement"},
         "uri": "https://opensource.org/licenses/BSD-3-Clause",
         "identifier": "BSD-3",
         "scheme": "BSD-3"
@@ -551,7 +329,7 @@ def test_license():
     assert data == valid_full
 
     valid_minimal = {
-        "license": "Copyright Maximo Decimo Meridio 2020. Long statement"
+        "license": {"en":"Copyright Maximo Decimo Meridio 2020. Long statement"}
     }
 
     data = LicenseSchemaV1().load(valid_minimal)
@@ -569,7 +347,7 @@ def test_license():
 def test_subject():
     """Test subject schema."""
     valid_full = {
-        "subject": "Romans",
+        "subject": {"cs":"Romans"},
         "identifier": "subj-1",
         "scheme": "no-scheme"
     }
@@ -578,7 +356,7 @@ def test_subject():
     assert data == valid_full
 
     valid_minimal = {
-        "subject": "Romans"
+        "subject": {"cs":"Romans"}
     }
 
     data = SubjectSchemaV1().load(valid_minimal)
@@ -639,7 +417,7 @@ def test_date():
         data = DateSchemaV1().load(invalid_end_format)
 
 
-def test_related_identifiers(vocabulary_clear):
+def test_related_identifiers():
     """Test related identifiers schema."""
     valid_full = {
         "identifier": "10.5281/zenodo.9999988",
@@ -786,7 +564,7 @@ def test_location():
             "lon": 12.496189
         },
         "place": "Rome",
-        "description": "Rome, from Romans"
+        "description": {"cs" : "neco"}
     }
 
     data = LocationSchemaV1().load(valid_full)
@@ -804,201 +582,18 @@ def test_location():
             "lat": 41.902604,
             "lon": 12.496189
         },
-        "description": "Rome, from Romans"
+        "description": {"cs" : "neco"}
     }
     with pytest.raises(ValidationError):
         data = LocationSchemaV1().load(invalid_no_place)
 
-
-def test_identifiers(minimal_input_record):
-    """Test Identifiers field."""
-    # No 'identifiers' field at all is supported
-    data = MetadataSchemaV1().load(minimal_input_record)
-    assert data.get('identifiers') == minimal_input_record.get('identifiers')
-
-    # Empty dict
-    minimal_input_record['identifiers'] = {}
-    data = MetadataSchemaV1().load(minimal_input_record)
-    assert data['identifiers'] == minimal_input_record['identifiers']
-
-    # Minimal
-    minimal_input_record['identifiers'] = {
-        "DOI": "10.5281/zenodo.9999999",
-    }
-    data = MetadataSchemaV1().load(minimal_input_record)
-    assert data['identifiers'] == minimal_input_record['identifiers']
-
-    # Different schemes
-    minimal_input_record['identifiers'] = {
-        "DOI": "10.5281/zenodo.9999999",
-        "ARK": "ark:/123/456",
-    }
-    data = MetadataSchemaV1().load(minimal_input_record)
-    assert data['identifiers'] == minimal_input_record['identifiers']
-
-    # With duplicate schemes, only last one is picked
-    minimal_input_record['identifiers'] = {
-        "DOI": "10.5281/zenodo.9999999",
-        "DOI": "10.5281/zenodo.0000000",
-    }
-    data = MetadataSchemaV1().load(minimal_input_record)
-    assert data['identifiers'] == minimal_input_record['identifiers']
-    assert data['identifiers']['DOI'] == "10.5281/zenodo.0000000"
-
-    # Invalid: no identifier
-    minimal_input_record['identifiers'] = {
-        "DOI": ""
+    invalid_no_place = {
+        "point": {
+            "lat": 41.902604,
+            "lon": 12.496189
+        },
+        "description": {"css": "neco"}
     }
     with pytest.raises(ValidationError):
-        data = MetadataSchemaV1().load(minimal_input_record)
+        data = LocationSchemaV1().load(invalid_no_place)
 
-    # Invalid: no scheme
-    minimal_input_record['identifiers'] = {
-        "": "10.5281/zenodo.9999999"
-    }
-    with pytest.raises(ValidationError):
-        data = MetadataSchemaV1().load(minimal_input_record)
-
-
-def test_extensions(app, minimal_input_record):
-    """Test metadata extensions schema."""
-    # Setup metadata extensions
-    RDM_RECORDS_METADATA_NAMESPACES = {
-        'dwc': {
-            '@context': 'https://example.com/dwc/terms'
-        },
-        'nubiomed': {
-            '@context': 'https://example.com/nubiomed/terms'
-        }
-    }
-
-    RDM_RECORDS_METADATA_EXTENSIONS = {
-        'dwc:family': {
-            'elasticsearch': 'keyword',
-            'marshmallow': SanitizedUnicode(required=True)
-        },
-        'dwc:behavior': {
-            'elasticsearch': 'text',
-            'marshmallow': SanitizedUnicode()
-        },
-        'nubiomed:number_in_sequence': {
-            'elasticsearch': 'long',
-            'marshmallow': Integer()
-        },
-        'nubiomed:scientific_sequence': {
-            'elasticsearch': 'long',
-            'marshmallow': List(Integer())
-        },
-        'nubiomed:original_presentation_date': {
-            'elasticsearch': 'date',
-            'marshmallow': DateString()
-        },
-        'nubiomed:right_or_wrong': {
-            'elasticsearch': 'boolean',
-            'marshmallow': Bool()
-        }
-    }
-
-    orig_metadata_extensions = (
-        app.extensions['invenio-rdm-records'].metadata_extensions
-    )
-
-    app.extensions['invenio-rdm-records'].metadata_extensions = (
-        MetadataExtensions(
-            RDM_RECORDS_METADATA_NAMESPACES,
-            RDM_RECORDS_METADATA_EXTENSIONS
-        )
-    )
-
-    # Minimal if not absent
-    valid_minimal = {
-        'dwc:family': 'Felidae'
-    }
-    minimal_input_record['extensions'] = valid_minimal
-    data = MetadataSchemaV1().load(minimal_input_record)
-    assert valid_minimal == data.get('extensions')
-
-    # Full
-    valid_full = {
-        'dwc:family': 'Felidae',
-        'dwc:behavior': 'Plays with yarn, sleeps in cardboard box.',
-        'nubiomed:number_in_sequence': 3,
-        'nubiomed:scientific_sequence': [1, 1, 2, 3, 5, 8],
-        'nubiomed:original_presentation_date': '2019-02-14',
-        'nubiomed:right_or_wrong': True,
-    }
-    minimal_input_record['extensions'] = valid_full
-    data = MetadataSchemaV1().load(minimal_input_record)
-    assert valid_full == data.get('extensions')
-
-    # Invalid
-    invalid_number_in_sequence = {
-        'dwc:family': 'Felidae',
-        'nubiomed:scientific_sequence': [1, 'l', 2, 3, 5, 8],
-    }
-    minimal_input_record['extensions'] = invalid_number_in_sequence
-    with pytest.raises(ValidationError):
-        data = MetadataSchemaV1().load(minimal_input_record)
-
-    app.extensions['invenio-rdm-records'].metadata_extensions = (
-        orig_metadata_extensions
-    )
-
-
-def test_publication_date(app, minimal_input_record, minimal_record):
-    def assert_publication_dates(data, expected):
-        assert data['publication_date'] == expected_record['publication_date']
-        assert (
-            data['_publication_date_search'] ==
-            expected_record['_publication_date_search']
-        )
-
-    expected_record = minimal_record
-
-    # Partial
-    minimal_input_record['publication_date'] = '2020-02'
-    expected_record['publication_date'] = '2020-02'
-    expected_record['_publication_date_search'] = '2020-02-01'
-
-    data = MetadataSchemaV1().load(minimal_input_record)
-
-    assert_publication_dates(data, expected_record)
-
-    # Interval (asymmetrical is allowed!)
-    minimal_input_record['publication_date'] = '2020-02-02/2025-12'
-    expected_record['publication_date'] = '2020-02-02/2025-12'
-    expected_record['_publication_date_search'] = '2020-02-02'
-
-    data = MetadataSchemaV1().load(minimal_input_record)
-
-    assert_publication_dates(data, expected_record)
-
-    # Invalid date
-    minimal_input_record['publication_date'] = 'invalid'
-    with pytest.raises(ValidationError):
-        data = MetadataSchemaV1().load(minimal_input_record)
-
-    # Invalid interval
-    minimal_input_record['publication_date'] = '2025-12/2020-02-02'
-    with pytest.raises(ValidationError):
-        data = MetadataSchemaV1().load(minimal_input_record)
-
-
-def test_embargo_date(vocabulary_clear, minimal_input_record):
-    # Test embargo validation
-    minimal_input_record["embargo_date"] = "1000-01-01"
-    with pytest.raises(ValidationError):
-        data = MetadataSchemaV1().load(minimal_input_record)
-
-
-def test_metadata_schema(
-        vocabulary_clear, full_input_record, full_record,
-        minimal_input_record, minimal_record):
-    """Test metadata schema."""
-    # Test full attributes
-    data = MetadataSchemaV1().load(full_input_record)
-    assert data == full_record
-
-    # Test minimal attributes
-    data = MetadataSchemaV1().load(minimal_input_record)
-    assert data == minimal_record
