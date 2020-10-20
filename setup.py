68cb2d995ed1f46104c4eb14a8ed8314a5c26e88
Alzbeta Pokorna <alzbeta.pokorna@cesnet.cz>
2020-10-20 18:23:59 +0200

    install requires

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ OAREPO_VERSION = os.environ.get('OAREPO_VERSION', '3.3.0')
 
 
 install_requires = [
-'oarepo-mapping-includes',
+    'oarepo-mapping-includes',
     'idutils',
     'edtf',
     'pycountry',
